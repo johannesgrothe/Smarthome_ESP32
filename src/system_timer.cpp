d27a7aa49836febfaa7a5898234622aecb484b79
Johannes Grothe <johannesgrothe@web.de>
2020-06-09 17:46:34 +0200

    #9 Refactored system_timer

---
+++ src/system_timer.cpp
@@ -0,0 +1,22 @@
+#include "system_timer.h"
+
+SystemTimer::SystemTimer() = default;
+
+unsigned long long SystemTimer::getTime() const {
+  return time_ + millis();
+}
+
+void SystemTimer::setTime(const unsigned long long new_time, const unsigned long time_offset = 0) {
+  time_ = new_time - millis() + time_offset;
+  logger.print("Setting System Time: ");
+  logger.add(new_time);
+  logger.add(" (+");
+  logger.add(time_offset);
+  logger.addln(")");
+  logger.incIndent();
+  logger.print(LOG_DATA, "System Launch Time: ");
+  logger.addln(time_);
+  logger.print(LOG_DATA, "Momentary System Time: ");
+  logger.addln(getTime());
+  logger.decIndent();
+}
--- src/system_timer.h
+++ src/system_timer.h
@@ -6,32 +6,17 @@
 
 class SystemTimer {
 private:
-  unsigned long long time = 0;
+  unsigned long long time_ = 0;
 
 public:
-  SystemTimer() {};
+  SystemTimer();
 
-  unsigned long long getTime() const {
-    return time + millis();
-  }
+  unsigned long long getTime() const;
 
-  void setTime(const unsigned long long new_time, const unsigned long time_offset=0) {
-    time = new_time - millis() + time_offset;
-    logger.print("Setting System Time: ");
-    logger.add(new_time);
-    logger.add(" (+");
-    logger.add(time_offset);
-    logger.addln(")");
-    logger.incIndent();
-    logger.print(LOG_DATA, "System Launch Time: ");
-    logger.addln(time);
-    logger.print(LOG_DATA, "Momentary System Time: ");
-    logger.addln(getTime());
-    logger.decIndent();
-  }
+  void setTime(unsigned long long, unsigned long);
 
 };
 
-SystemTimer system_timer;
+static SystemTimer system_timer;
 
 #endif //__SystemTimer__
