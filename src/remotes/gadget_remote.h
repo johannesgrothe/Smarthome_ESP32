308c526bafa5ad04f9b670397b35d8eac3389c34
Johannes Grothe <johannesgrothe@web.de>
2020-06-24 16:18:45 +0200

    #9 Refactored code_remote and gadget_remote

--- src/remotes/gadget_remote.h
+++ src/remotes/gadget_remote.h
@@ -11,43 +11,21 @@ class GadgetRemote: public Remote {
 private:
 
   virtual bool
-  registerGadget(const char *gadget_name, Gadget_Type gadget_type, const char *characteristics) { return false; };
+  registerGadget(const char *gadget_name, Gadget_Type gadget_type, const char *characteristics);
 
-  virtual bool removeGadget(const char *gadget_name) { return false; };
+  virtual bool removeGadget(const char *gadget_name);
 
-  bool registerGadgetOnRemote(const char *gadget_name, Gadget_Type gadget_type, const char *characteristics) {
-    logger.println("Registering Gadget:");
-    logger.incIndent();
-    if (registerGadget(gadget_name, gadget_type, characteristics)) {
-      logger.println(LOG_INFO, "OK");
-      logger.decIndent();
-      return true;
-    } else {
-      logger.println(LOG_ERR, "ERR");
-      logger.decIndent();
-    }
-    return false;
-  }
+  bool registerGadgetOnRemote(const char *gadget_name, Gadget_Type gadget_type, const char *characteristics);
 
-  bool handleNewGadget(SH_Gadget * new_gadget) override {
-    char characteristic_str[HOMEBRIDGE_REGISTER_STR_MAX_LEN - 60];
-    new_gadget->getCharacteristics(&characteristic_str[0]);
-    if (registerGadgetOnRemote(new_gadget->getName(), new_gadget->getType(), characteristic_str))
-      logger.println("Registering Gadget successfull.");
-    else
-      logger.println(LOG_ERR, "Failed to register Gadget.");
-    return true;
-  }
+  bool handleNewGadget(SH_Gadget * new_gadget) override;
 
 public:
-  explicit GadgetRemote(JsonObject data) :
-    Remote(data) {};
+  explicit GadgetRemote(JsonObject data);
 
-  GadgetRemote(Request_Gadget *gadget, JsonObject data) :
-    Remote(gadget, data) {};
+  GadgetRemote(Request_Gadget *gadget, JsonObject data);
 
   virtual void
-  updateCharacteristic(const char *gadget_name, const char *service, const char *characteristic, int value) {};
+  updateCharacteristic(const char *gadget_name, const char *service, const char *characteristic, int value);
 
 };
 
