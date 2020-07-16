73344433c601ad0bbc1b5dddca8f85c3ea40fc36
Johannes Grothe <johannesgrothe@web.de>
2020-07-16 14:19:11 +0200

    #9 Refactored remote

--- src/remotes/remote.h
+++ src/remotes/remote.h
@@ -14,23 +14,16 @@ private:
   bool lock_updates;
   bool network_initialized;
 
-
 protected:
 
   Request_Gadget *req_gadget;
   Gadget_Collection gadgets;
 
-  bool updatesAreLocked() const { return lock_updates; }
+  bool updatesAreLocked() const;
 
-  void lockUpdates() {
-    logger.println("Locking Updates");
-    lock_updates = true;
-  }
+  void lockUpdates();
 
-  void unlockUpdates() {
-    logger.println("Unlocking Updates");
-    lock_updates = false;
-  }
+  void unlockUpdates();
 
   virtual void handleRequest(const char *path, const char *body) = 0;
 
@@ -38,49 +31,16 @@ protected:
 
   virtual bool handleNewGadget(SH_Gadget *new_gadget) = 0;
 
-  bool isNetworkInitialised() {
-    return network_initialized;
-  }
+  bool isNetworkInitialised();
 
 public:
-  explicit Remote(JsonObject data) :
-    lock_updates(false),
-    network_initialized(false),
-    req_gadget(nullptr),
-    gadgets(){};
+  explicit Remote(JsonObject data);
 
-  explicit Remote(Request_Gadget *gadget, JsonObject data) :
-      lock_updates(false),
-      network_initialized(true),
-      req_gadget(gadget),
-      gadgets(){};
+  explicit Remote(Request_Gadget *gadget, JsonObject data);
 
-  void handleRequest(Request *req) {
-    DynamicJsonDocument body_json(2048);
-    DeserializationError err = deserializeJson(body_json, req->getBody());
-    if (err == DeserializationError::Ok) {
-      JsonObject body = body_json.as<JsonObject>();
-      handleRequest(req->getPath(), body);
-    } else {
-      handleRequest(req->getPath(), req->getBody());
-    }
-  };
+  void handleRequest(Request *req);
 
-  void addGadget(SH_Gadget *new_gadget) {
-    if (gadgets.addGadget(new_gadget)) {
-      logger.print(LOG_DATA, "Adding '");
-      logger.add(new_gadget->getName());
-      logger.addln("'");
-
-      logger.incIndent();
-      handleNewGadget(new_gadget);
-      logger.decIndent();
-    } else {
-      logger.print(LOG_ERR, "Unable to add '");
-      logger.add(new_gadget->getName());
-      logger.addln("'");
-    }
-  }
+  void addGadget(SH_Gadget *new_gadget);
 };
 
 #endif //__Remote__
