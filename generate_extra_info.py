1b39554dadd9a662a03579ef1be2e4bfff7336b4
Johannes Grothe <johannesgrothe@web.de>
2020-12-27 15:15:25 +0100

    #27 Changed SW_FLASH_TIME format

--- generate_extra_info.py
+++ generate_extra_info.py
@@ -1,5 +1,5 @@
 import os
-from datetime import date
+from datetime import datetime
 
 file_name = "flash_info.h"
 
@@ -8,14 +8,23 @@ print("Generating '{}' file to define flashing info...".format(file_name))
 data_str = "#pragma once\n// THIS IS AN AUTO_GENERATED FILE.\n// TOUCH IT IF YOU WANT, THE GENERATOR DOESNT CARE.\n\
 // BUT HE HAS NO MERCY EITHER.\n"
 
-flash_time = '#define SW_FLASH_TIME "{}"\n'.format(date.today())
-data_str += flash_time
 
-git_commit = '#define SW_GIT_COMMIT "{}"\n'.format(os.popen("git rev-parse HEAD").read().strip("\n"))
-data_str += git_commit
+flash_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+flash_time_str = '#define SW_FLASH_TIME "{}"\n'.format(flash_time)
+data_str += flash_time_str
 
-git_branch = '#define SW_GIT_BRANCH "{}"\n'.format(os.popen("git for-each-ref --format='%(upstream:short)' $(git symbolic-ref -q HEAD)").read().strip("\n"))
-data_str += git_branch
+git_commit = os.popen("git rev-parse HEAD").read().strip("\n")
+git_commit_str = '#define SW_GIT_COMMIT "{}"\n'.format(git_commit)
+data_str += git_commit_str
+
+git_branch = os.popen("git for-each-ref --format='%(upstream:short)' $(git symbolic-ref -q HEAD)").read().strip("\n")
+git_branch_str = '#define SW_GIT_BRANCH "{}"\n'.format(git_branch)
+data_str += git_branch_str
+
+print("Data:")
+print("   Time: {}".format(flash_time))
+print("   Commit: {}".format(git_commit))
+print("   Branch: {}".format(git_branch))
 
 print("Saving...")
 
