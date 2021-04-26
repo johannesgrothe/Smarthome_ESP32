ced4979135a8bfc31129a7e118e706534bea1250
Johannes Grothe <johannesgrothe@web.de>
2021-04-26 18:01:12 +0200

    #40 forcing clang compiler via python

--- test_static/run_static_tests.py
+++ test_static/run_static_tests.py
@@ -1,16 +1,34 @@
 import os
 import sys
+import argparse
+from typing import Optional
 
 
-def main() -> bool:
+def run_tests(force_compiler: Optional[str] = None) -> bool:
     if not os.path.isdir("build"):
         os.system("mkdir build")
-    success = os.system("cd build && cmake .. && make && ./static_tests") == 0
+
+    compiler_select = " "
+    if force_compiler:
+        compiler_select = f" -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang "
+    success = os.system(f"cd build && cmake .. && make{compiler_select}&& ./static_tests") == 0
     return success
 
 
+def parse_args():
+    """Parses the arguments given to the script"""
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--compiler', type=str, help='Compiler to use')
+    return parser.parse_args()
+
+
 if __name__ == "__main__":
-    test_status = main()
+    args = parse_args()
+    
+    if args.compiler:
+        test_status = run_tests(args.compiler)
+    else:
+        test_status = run_tests()
 
     if test_status:
         sys.exit(0)
