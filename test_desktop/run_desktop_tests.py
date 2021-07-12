0c39ed7d013bf51e7353c8aaeffd57baae2f01a4
Johannes Grothe <johannesgrothe@web.de>
2021-07-12 18:28:35 +0200

    #45 Added base for code coverage

--- test_desktop/run_desktop_tests.py
+++ test_desktop/run_desktop_tests.py
@@ -17,8 +17,10 @@ def run_tests(force_compiler: Optional[str] = None, clean: bool = False) -> bool
     if force_compiler:
         compiler_select = f" -DCMAKE_C_COMPILER={force_compiler} -DCMAKE_CXX_COMPILER={force_compiler} "
         print(f"Forcing '{force_compiler}' compiler")
+    # success = os.system(f"cd build && cmake .. -DUSE_GCOV=yes {compiler_select}&& make") == 0
     success = os.system(f"cd build && cmake ..{compiler_select}&& make") == 0
     if success:
+        # success = os.system("make all && make tests && make coverage")
         xml_option = "--output-junit ../test_reports/desktop_tests.xml"  # TODO: not happenin
         log_option = "--output-log ../test_reports/desktop_tests.log"
         success = os.system(f"ctest --test-dir build {xml_option} {log_option}")
