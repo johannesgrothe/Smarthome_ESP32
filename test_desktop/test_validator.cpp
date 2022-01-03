e14614e47d9ef93d2f9f92cf506b98294dd5f46f
Johannes Grothe <johannesgrothe@web.de>
2022-01-03 22:05:35 +0100

    #57 Added library import and empty test cast

---
+++ test_desktop/test_validator.cpp
@@ -0,0 +1,12 @@
+#include "catch.hpp"
+
+#include <string>
+#include <memory>
+#include "validator.h"
+
+TEST_CASE("Test JSON Validator", "[Helpers]") {
+
+  SECTION("Test Empty") {
+    CHECK(true);
+  }
+}
---
+++ test_desktop/validator.h
@@ -0,0 +1,7 @@
+#pragma once
+
+// Use schemas from system submodule and the validator repo to create a validator class
+// validator transforms arduinoJson into json objects and validates them
+// https://github.com/pboettch/json-schema-validator
+
+#include <nlohmann/json-schema.hpp>
