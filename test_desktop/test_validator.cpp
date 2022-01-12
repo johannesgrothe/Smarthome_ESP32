0d94ffe6dc5c8adcf675b84c14da1d60a33ab678
Johannes Grothe <johannesgrothe@web.de>
2022-01-12 21:00:07 +0100

    #57 Finished test

--- test_desktop/test_validator.cpp
+++ test_desktop/test_validator.cpp
@@ -5,7 +5,11 @@
 TEST_CASE("Test JSON Validator", "[Helpers]") {
 
   Validator validator;
-  nlohmann::json test_json = R"({"status":"everything ok","ack": true})";
+  nlohmann::json test_json = R"({"status":"everything ok","ack": true})"_json;
+  nlohmann::json test_json_broken = R"({"status":"everything ok","ack": 133})"_json;
+
+  DynamicJsonDocument test_json_arduino(7000);
+  deserializeJson(test_json_arduino, R"({"status":"everything ok","ack": true})");
 
   std::stringstream buf_str;
   buf_str << test_json;
@@ -13,5 +17,11 @@ TEST_CASE("Test JSON Validator", "[Helpers]") {
 
   SECTION("Test verify") {
     CHECK(validator.validate(test_json, "default_message.json"));
+    CHECK(!validator.validate(test_json_broken, "default_message.json"));
+    CHECK(!validator.validate(test_json, "api_config_save.json"));
+  }
+
+  SECTION("Test verify ArduinoJson") {
+    CHECK(validator.validate(test_json_arduino, "default_message.json"));
   }
 }
